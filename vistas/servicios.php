544880586afe3edcd82cbc323e1992b770749646
RicardoMaIdonado <65556285+RicardoMaIdonado@users.noreply.github.com>
2020-08-06 10:15:49 -0500

    Update servicios.php

--- vistas/servicios.php
+++ vistas/servicios.php
@@ -44,7 +44,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="noticias.php">
+                    <a class="nav-link" href="https://webvaince.herokuapp.com/vistas/noticias.php">
                         <div style="color:white;">Noticias</div>
                     </a>
                 </li>
