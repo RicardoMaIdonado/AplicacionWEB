23321e882b7f27c92405d87cc558906035741e66
RicardoMaIdonado <65556285+RicardoMaIdonado@users.noreply.github.com>
2020-08-06 22:45:10 -0500

    Update servicios.php

--- vistas/servicios.php
+++ vistas/servicios.php
@@ -111,7 +111,7 @@
                     <div class="card">
                         <div class="embed-responsive embed-responsive-16by9 mb-4">
                             <?php
-                            $API_KEY = "AIzaSyDfNf4tScLLMwigixb5Htjejg8smQ7CpD0";
+                            $API_KEY = "";
                             $uri_relativa = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=' . urlencode($name) . '%20champion%20spotlight&key=' . $API_KEY;
                             $video_lista = json_decode(file_get_contents($uri_relativa), true);
                             $vidID = $video_lista['items'][0]['id']['videoId'];
